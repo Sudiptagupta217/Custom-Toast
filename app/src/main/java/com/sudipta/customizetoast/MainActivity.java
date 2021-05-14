33e6f186b562c7a295ca6ae9098edd5adbbba0c8
Sudipta Gupta <sudiptagupta217@gmail.com>
2021-05-14 20:40:32 +0530

    add Custom Toast

--- app/src/main/java/com/sudipta/customizetoast/MainActivity.java
+++ app/src/main/java/com/sudipta/customizetoast/MainActivity.java
@@ -2,7 +2,9 @@ package com.sudipta.customizetoast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Toast;
 
@@ -14,6 +16,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public void SimpleToast(View view){
-        Toast.makeText(this, "Hi,this is basic Toast", Toast.LENGTH_SHORT).show();
+//        Toast.makeText(this, "Hi,this is basic Toast", Toast.LENGTH_SHORT).show();
+        Context context =getApplicationContext();
+        LayoutInflater inflater = getLayoutInflater();
+
+        View customToastRoot = inflater.inflate(R.layout.my_toast,null);
+        Toast customToast = new Toast(context);
+
+        customToast.setView(customToastRoot);
+        customToast.setDuration(Toast.LENGTH_LONG);
+        customToast.show();
+
     }
 }
